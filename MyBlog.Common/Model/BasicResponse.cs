01ea6a71309ba5b9d3fba21d6fbedc7b4b77ed95
zhangcheng25@lenovo.com <zhangcheng25@lenovo.com>
2024-06-03 18:11:51 +0800

    工程初始化

---
+++ MyBlog.Common/Model/BasicResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.Common.Model
+{
+    public class BasicResponse<T>
+    {
+        public string Code { get; set; }
+
+        public T Data { get; set; }
+
+        public string Message { get; set; }        
+    }
+}
---
+++ MyBlog.Common/readme.md
@@ -0,0 +1,4 @@
+﻿### Common 目录
++ model 基础的model
++ util 基础工具
++ 

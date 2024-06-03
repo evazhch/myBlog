40d8a2b726108e6ff013cdcc17e529ea70394eeb
evazhch <zhangcheng6013@live.com>
2024-06-03 23:06:30 +0800

    基本

---
+++ MyBlog.Common/Model/BasicPageResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.Common.Model
+{
+    public  class BasicPageResponse<T>
+    {
+        public int PageIndex {  get; set; }
+
+        public int PageCount { get; set; }
+
+        public int Total { get; set; }
+
+        public List<T> Rows { get; set; }
+    }
+}
